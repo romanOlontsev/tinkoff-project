45343bdedb744a8eea79dccaabc1959755a7299f
romanOlontsev <roverviner@yandex.ru>
2023-03-26 23:41:32 +0300

    minor changes

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/model/request/RemoveLinkRequest.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/model/request/RemoveLinkRequest.java
@@ -1,70 +1,8 @@
 package ru.tinkoff.edu.java.scrapper.model.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.validation.annotation.Validated;
+import lombok.Data;
 
-import java.util.Objects;
-
-@Validated
+@Data
 public class RemoveLinkRequest {
-    @JsonProperty("link")
-    private String link = null;
-
-    public RemoveLinkRequest link(String link) {
-        this.link = link;
-        return this;
-    }
-
-    /**
-     * Get link
-     * @return link
-     **/
-    @Schema
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        RemoveLinkRequest removeLinkRequest = (RemoveLinkRequest) o;
-        return Objects.equals(this.link, removeLinkRequest.link);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(link);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class RemoveLinkRequest {\n");
-
-        sb.append("    link: ").append(toIndentedString(link)).append("\n");
-        sb.append("}");
-        return sb.toString();
-    }
-
-    /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
-     */
-    private String toIndentedString(Object o) {
-        if (o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
-    }
+    private String link;
 }
--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/model/response/ListLinksResponse.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/model/response/ListLinksResponse.java
@@ -1,27 +1,15 @@
 package ru.tinkoff.edu.java.scrapper.model.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.Valid;
-import org.springframework.validation.annotation.Validated;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
-@Validated
+@Data
 public class ListLinksResponse {
-    @JsonProperty("links")
-    @Valid
-    private List<LinkResponse> links = null;
 
-    @JsonProperty("size")
-    private Integer size = null;
+    private List<LinkResponse> links;
 
-    public ListLinksResponse links(List<LinkResponse> links) {
-        this.links = links;
-        return this;
-    }
+    private Integer size;
 
     public ListLinksResponse addLinksItem(LinkResponse linksItem) {
         if (this.links == null) {
@@ -29,79 +17,5 @@ public class ListLinksResponse {
         }
         this.links.add(linksItem);
         return this;
-    }
-
-    /**
-     * Get links
-     * @return links
-     **/
-    @Schema
-    @Valid
-    public List<LinkResponse> getLinks() {
-        return links;
-    }
-
-    public void setLinks(List<LinkResponse> links) {
-        this.links = links;
-    }
-
-    public ListLinksResponse size(Integer size) {
-        this.size = size;
-        return this;
-    }
-
-    /**
-     * Get size
-     * @return size
-     **/
-    @Schema
-
-    public Integer getSize() {
-        return size;
-    }
-
-    public void setSize(Integer size) {
-        this.size = size;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        ListLinksResponse listLinksResponse = (ListLinksResponse) o;
-        return Objects.equals(this.links, listLinksResponse.links) &&
-                Objects.equals(this.size, listLinksResponse.size);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(links, size);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class ListLinksResponse {\n");
-
-        sb.append("    links: ").append(toIndentedString(links)).append("\n");
-        sb.append("    size: ").append(toIndentedString(size)).append("\n");
-        sb.append("}");
-        return sb.toString();
-    }
-
-    /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
-     */
-    private String toIndentedString(Object o) {
-        if (o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
     }
 }
