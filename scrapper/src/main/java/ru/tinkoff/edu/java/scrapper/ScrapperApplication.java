7dc11a40cc731a25d642a41fb2ee74a0b8cfa710
romanOlontsev <roverviner@yandex.ru>
2023-04-05 14:09:04 +0300

    added support for TestContainers with liquibase table migration, added container test

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -2,11 +2,12 @@ package ru.tinkoff.edu.java.scrapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
     public static void main(String[] args) {
---
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/container/IntegrationEnvironment.java
@@ -0,0 +1,23 @@
+package ru.tinkoff.edu.java.scrapper.container;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public abstract class IntegrationEnvironment {
+
+    private static final PostgreSQLContainer<?> postgres;
+
+    static {
+        postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15"));
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void datasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.username", postgres::getUsername);
+    }
+}
