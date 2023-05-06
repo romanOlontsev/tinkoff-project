5fe22dd43ac6c9ee7b01e935c56cbf728e4d6303
romanOlontsev <roverviner@yandex.ru>
2023-05-06 17:33:56 +0300

    minor changes

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -13,6 +13,5 @@ public class ScrapperApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
-        System.out.println(config);
     }
 }
