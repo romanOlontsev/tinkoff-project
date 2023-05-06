9cea356b79d422df7fcee567ec32a412c4c756f0
romanOlontsev <roverviner@yandex.ru>
2023-05-06 17:32:37 +0300

    added checkstyle job

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -13,5 +13,6 @@ public class ScrapperApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
+        System.out.println(config);
     }
 }
